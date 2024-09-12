af72e4116ec2a668fd5a7e36adb912ffddc4c623
André Brandão <andrebf4@gmail.com>
2024-09-12 16:02:00 -0300

    chore: tailwind added. testing styles on githubpages

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="body">
+    
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'André Brandão - FullStack Developer',
+  ogTitle: 'André Brandão - FullStack Developer',
+  description: 'André Brandão - FullStack Developer',
+  ogDescription: 'André Brandão - FullStack Developer',
+})
+</script>
+
+<style>
+.body {
+  @apply
+  w-full
+  min-h-[100vh]
+  overflow-x-hidden
+  overflow-y-auto
+  bg-gradient-to-r
+  from-slate-900 
+  to-slate-700
+}
+</style>
